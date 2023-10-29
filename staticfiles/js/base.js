1b7d6d1d9e6d1c7341aa85cc0318e498915ec744
Anthony Smith <smitht061@gmail.com>
2023-10-29 17:03:08 -0400

    putting script into js file

---
+++ staticfiles/js/base.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const randomizeButton = document.getElementById('randomize-stats')
+    randomizeButton.addEventListener('click', function () {
+      // Call the backend view to generate random stats
+      fetch('/character/generate_random_stats/')
+        .then((response) => response.json())
+        .then((data) => {
+          // Update the form fields with the random stats
+          document.getElementById('id_strength').value = data.strength
+          document.getElementById('id_dexterity').value = data.dexterity
+          document.getElementById('id_constitution').value = data.constitution
+          document.getElementById('id_intelligence').value = data.intelligence
+          document.getElementById('id_wisdom').value = data.wisdom
+          document.getElementById('id_charisma').value = data.charisma
+        })
+    })
+  })
