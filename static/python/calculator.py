965ff53800693b3253c814285203e085d6e50261
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2023-11-06 08:47:19 -0500

    adding pyscript app

--- static/python/calculator.py
+++ static/python/calculator.py
@@ -1,8 +1,26 @@
+from pyscript import Element
 
-import pandas
 
 def add(numbers, sum=0):
     for number in numbers:
         sum += number
 
     return sum
+
+
+def subtract(numbers, difference=0):
+    for number in numbers:
+        difference += number
+
+    return difference
+
+
+input = Element("input")
+button = Element("button")
+
+
+def hello_world(input):
+    input.write(f"Hello world")
+
+
+hello_world(input)
--- staticfiles/python/calculator.py
+++ staticfiles/python/calculator.py
@@ -1,4 +1,4 @@
-from pyweb import pydom
+from pyscript import window, document
 
 
 def add(numbers, sum=0):
@@ -6,3 +6,16 @@ def add(numbers, sum=0):
         sum += number
 
     return sum
+
+
+def subtract(numbers, difference=0):
+    for number in numbers:
+        difference += number
+
+    return difference
+
+
+input = document.querySelector("input")
+button = document.querySelector("button")
+
+print(input)
